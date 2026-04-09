15079d766ae4b9031d454a5f11d7c01229779636
eemmtt <etmuzio@gmail.com>
2026-04-09 14:34:09 -0400

    impl delete habit button

--- client/app/routes/habit.tsx
+++ client/app/routes/habit.tsx
@@ -1,5 +1,5 @@
 import type { RouteHandle } from "~/types";
-import { useParams } from "react-router";
+import { redirect, useFetcher, useParams } from "react-router";
 import type { Route } from "./+types/habit";
 
 export const handle: RouteHandle = { title: "Habit Details", parent: "/" };
@@ -11,12 +11,44 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
+export async function action({ request }: Route.ActionArgs) {
+  const formData = await request.formData();
+  const intent = formData.get("intent");
+  const cookie = request.headers.get("cookie") ?? "";
+
+  if (intent === "delete-habit") {
+    const habit_id = formData.get("habit_id") ?? "";
+    const rest = await fetch(
+      `${process.env.API_URL}api/habits/${habit_id}/delete`,
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json", cookie },
+        body: "",
+      },
+    );
+    if (rest.ok) return redirect("/");
+  }
+}
+
 export default function Habit() {
   const { id } = useParams();
+  const fetcher = useFetcher();
   return (
     <main className="p-4">
       <h1>{id}</h1>
       <p>Todo: display habit details</p>
+      <button
+        className="text-sm cursor-pointer w-fit px-1 focus:outline focus:outline-primary"
+        onClick={async (e) => {
+          e.preventDefault();
+          fetcher.submit(
+            { intent: "delete-habit", habit_id: id ?? "" },
+            { method: "POST" },
+          );
+        }}
+      >
+        Delete Habit
+      </button>
     </main>
   );
 }
